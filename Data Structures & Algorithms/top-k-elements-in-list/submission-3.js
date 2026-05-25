a2f66cb1292c5af32bba1887c2171b68a4ea90a3
Dhyan M Patel <137033217+DhyanMPatel@users.noreply.github.com>
2026-05-25 16:36:57 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const prevMap = new Map()
+        const res = [];
+
+        for(let i = 0; i<nums.length; i++) {
+            if(prevMap.has(nums[i])) {
+                prevMap.set(nums[i], prevMap.get(nums[i]) + 1)
+            } else {
+                prevMap.set(nums[i], 1)
+            }
+        }
+
+        return [...prevMap.entries()].sort((a, b) => b[1] - a[1]).slice(0,k).map(item => item[0])
+
+
+        /**
+         * The below value returns an array of elements whose frequency of appearance is greater than k.
+         */
+
+        // prevMap.forEach((value, index) => {
+        //     if(value >= k) {
+        //         res.push(index)
+        //     }
+        // })
+
+        // return res
+
+    }
+}
