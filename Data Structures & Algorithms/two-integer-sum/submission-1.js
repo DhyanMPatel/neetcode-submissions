798110e500b9e36a8f0f312ed0e9b5da80c01db5
Dhyan M Patel <137033217+DhyanMPatel@users.noreply.github.com>
2026-05-25 16:10:07 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const prevMap = new Map();
+
+        for(let i = 0; i< nums.length; i++) {
+            const diff = target - nums[i]
+            if(prevMap.has(diff)) {
+                return [prevMap.get(diff), i]
+            }
+            prevMap.set(nums[i], i)
+        }
+        return [];
+    }
+
+    /**
+     * Her we can't use Set(). If want to use then conplexity will be O(n^2)
+     * Because we need index of arr element which is equal to target
+     * Instead of that if we want "2 sum of element == target" is awailable then Set will become easy.
+     */
+}
